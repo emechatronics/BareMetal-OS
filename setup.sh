7576611068e788d7a6d79fc52f09e7cc7aab4bec
fragomr <javigorod@gmail.com>
2017-09-05 23:48:38 +0200

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -7,19 +7,19 @@ mkdir -p src
 mkdir -p bin
 
 if [ ! -e src/BMFS ]; then
-  git clone https://github.com/ReturnInfinity/BMFS.git src/BMFS
+  git clone https://github.com/fragomr/BMFS.git src/BMFS
 else
   git --git-dir=src/BMFS/.git pull origin master
 fi
 
 if [ ! -e src/Pure64 ]; then
-  git clone https://github.com/ReturnInfinity/Pure64.git src/Pure64
+  git clone https://github.com/fragomr/Pure64.git src/Pure64
 else
   git --git-dir=src/Pure64/.git pull origin master
 fi
 
 if [ ! -e src/BareMetal-OS ]; then
-  git clone https://github.com/ReturnInfinity/BareMetal-kernel.git src/BareMetal-kernel
+  git clone https://github.com/fragomr/BareMetal-kernel.git src/BareMetal-kernel
 else
   git --git-dir=src/BareMetal-kernel/.git pull origin master
 fi
