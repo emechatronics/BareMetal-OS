c0df91c4faed2a994279ac8dca97fde687b03ad4
fragomr <javigorod@gmail.com>
2017-09-06 00:16:24 +0200

    Add files via upload

--- newlib.sh
+++ newlib.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if [ ! -e src/BareMetal-newlib ]; then
-  git clone https://github.com/ReturnInfinity/BareMetal-newlib.git src/BareMetal-newlib
+  git clone https://github.com/fragomr/BareMetal-newlib.git src/BareMetal-newlib
 else
   git --git-dir=src/BareMetal-newlib/.git pull origin master
 fi
 
 cd src/BareMetal-newlib
-./build-newlib.sh
+./build-newlib.sh -j8
