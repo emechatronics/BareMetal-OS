5e6f0aadf34d4039e48892f843833085774901f5
fragomr <javigorod@gmail.com>
2017-09-06 12:21:27 +0200

    Add files via upload

--- appc.sh
+++ appc.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-cd src/BareMetal-OS/programs/
+cd programs/
 gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -o $1.o $1.c
 gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -o libBareMetal.o libBareMetal.c
 ld -T app.ld -o $1.app $1.o libBareMetal.o
-mv $1.app ../../../bin/
+mv $1.app ../bin/
 if [ $? -eq 0 ]; then
-cd ../../../bin
+cd ../bin
 ./bmfs bmfs.image create $1.app 2
 ./bmfs bmfs.image write $1.app
 cd ..
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cd bin
-qemu-system-x86_64 -smp 4 -m 256 -drive id=disk,file=bmfs.image,if=none,format=raw -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 -name "BareMetal OS" -net nic,model=e1000 -curses
+qemu-system-x86_64 -smp 1 -m 256 -drive id=disk,file=bmfs.image,if=none,format=raw -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 -name "BareMetal OS" -net nic,model=e1000 -curses -boot d 
+-vga std
